a31c4ffb249a9c3c55923e9b535b920b23d546ee
Patryk Czekaj <patryk13290@gmail.com>
2024-10-04 23:02:35 +0200

    create vertex array

--- src/main.cpp
+++ src/main.cpp
@@ -94,6 +94,9 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
     /* Create a windowed mode window and its OpenGL context */
@@ -124,13 +127,19 @@ int main(void)
 		2, 3, 0
 	};
 
+    unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+    //Create buffer that stores positions
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    //Specify how positions are stored
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
     unsigned int ibo;
     glGenBuffers(1, &ibo);
@@ -142,13 +151,30 @@ int main(void)
 
     ParseShader("./res/shaders/Basic.shader", vertexShader, fragmentShader);
 
-    unsigned int program = CreateShaders(vertexShader, fragmentShader);
-    glUseProgram(program);
+    unsigned int shader = CreateShaders(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+    int location = glGetUniformLocation(shader, "u_Color");
+    ASSERT(location != -1);
+    glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f);
+
+    glBindVertexArray(0);
+    glUseProgram(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
@@ -158,7 +184,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-    glDeleteProgram(program);
+    glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
